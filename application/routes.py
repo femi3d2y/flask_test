8316cac8c0730ab8c9e9f654275ca753c89d4a6c
femi3d2y <ifeolu.fayanju@academytrainee.com>
2019-11-28 14:00:13 +0000

    Add links and data

--- application/routes.py
+++ application/routes.py
@@ -4,10 +4,23 @@ from application import app
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home')
+    return render_template('home.html', title='Home', posts=dummyData)
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
 @app.route('/login')
 def login():
     return render_template('login.html', title='Login')
+
+dummyData = [
+        {
+            "name": {"first":"Leeroy", "last":"Jenkins"},
+            "title":"First Post",
+            "content":"This is some dummy data for Flask lectures"
+        },
+        {
+            "name": {"first":"Matthew", "last":"Patel"},
+            "title":"Second Post",
+            "content":"This is even more dummy data for Flask lectures"
+        }
+    ]
