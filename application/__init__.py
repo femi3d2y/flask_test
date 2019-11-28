e37de0ee0087ad115821a560948e13f2547ddf91
femi3d2y <ifeolu.fayanju@academytrainee.com>
2019-11-28 16:56:21 +0000

    kind of works

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:aerojak@35.242.176.146/db'
+db = SQLAlchemy(app)
 
 from application import routes
